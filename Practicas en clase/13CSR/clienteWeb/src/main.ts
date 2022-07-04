2e94101a4970cdd9222720d7ba9cbe8851d97f98
Aldolc30 <aldolino11@gmail.com>
2022-07-04 08:16:31 -0500

    update clase csr

--- Practicas en clase/13CSR/clienteWeb/src/main.ts
+++ Practicas en clase/13CSR/clienteWeb/src/main.ts
@@ -82,13 +82,51 @@ consultar.addEventListener('click', async ()=>{
     cuerpo.innerHTML="";
     cuerpo.appendChild(tabla);
     document.querySelectorAll('.boton').forEach( (ele:Element) =>{
-      (ele as HTMLButtonElement).addEventListener('click', ()=>{
-        httpAxios.get(`productos/id`)
+      ele.addEventListener('click', async()=>{
+        const {data} = await httpAxios.get(`productos/${(ele as HTMLButtonElement).value}`)
+        console.log(data);
+        nombre.value = data.nombre;
+        precio.value = data.precio.toString();
+        costo.value = data.costo.toString();
+        minimo.value = data.minimo.toString();
+        stock.value = data.stock.toString();
+        estado.value = data.estado!.toString();
+        id.value = data._id! 
         
       })
     })
 })
 
 
+const asignarValores =()=>{
+  const data: Producto = {
+    nombre: nombre.value,
+    costo: Number(costo.value),
+    precio: Number(precio.value),
+    minimo: Number(minimo.value),
+    stock: Number(stock.value),
+  }
+  return data;
+}
+
+grabar.addEventListener('click', async ()=>{
+  const data = asignarValores()
+  // trim elimina los espacios en blancos a la derecha e izquierda
+  if (id.value.trim().length>0){
+    const respproducto:Producto =  await (await httpAxios.put<Producto>(`productos/${id.value}`,data)).data
+    console.log(`El producto ${respproducto.nombre} fue modificado con éxito`);
+    return
+  }
+  try{
+    const respproducto:Producto = await (await httpAxios.post<Producto>(`productos`,data)).data
+    console.log(`El producto ${respproducto.nombre} fue insertado con éxito`);
+  }catch (error) {
+    if(axios.isAxiosError(error))
+    {
+      console.log(`Error en axios`);
+    }
+    console.log(error);
+  }
+})
 
 
